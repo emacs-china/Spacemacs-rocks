9f33602c356a8d465d1a3a66fb53cf609c667b75
zilongshanren <guanghui8827@126.com>
2016-03-21 00:00:35 +0800

    Spacemacs rocks episode2 day1

---
+++ episode2/init.el
@@ -0,0 +1,41 @@
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+(electric-indent-mode -1)
+(setq inhibit-splash-screen t)
+;- turn off tool-bar
+;- turn off scroll-bar
+;- show linum-mode
+;- turn off splash screen
+;- save your config
+;- define a function to quickly open your config file.
+
+(global-linum-mode t)
+
+(defun open-my-init-file()
+  (interactive)
+  (find-file "~/.emacs.d/init.el"))
+
+(global-set-key (kbd "<f2>") 'open-my-init-file)
+
+(global-company-mode t)
+(setq cursor-type 'bar)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (company))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
