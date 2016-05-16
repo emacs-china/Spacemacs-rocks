df95b9d4d436cc975834c49bcf233c74acb28517
zilongshanren <guanghui8827@126.com>
2016-05-17 07:46:25 +0800

    add day8

--- Season2/day1/init.el
+++ Season2/day1/init.el
@@ -1,4 +1,3 @@
-
 ;; Added by Package.el.  This must come before configurations of
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
