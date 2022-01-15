8aeed5eb0ed4c53caad2f177417ea728042f3564
zilongshanren <guanghui8827@gmail.com>
2022-01-15 13:32:37 +0800

    modify deploy command for local publish

--- deploy.sh
+++ deploy.sh
@@ -6,13 +6,13 @@ rm -rf public
 mkdir public
 
 # config
-git config --global user.email "nobody@li-xinyang.com"
-git config --global user.name "Travis CI"
+#git config --global user.email "nobody@li-xinyang.com"
+#git config --global user.name "Travis CI"
 
 # make
 emacs README.org --batch --eval="(load-file \"generate_readme.el\")" -f org-html-export-to-html --kill
 mv README.html ./public/index.html
-mv readtheorg ./public/readtheorg/
+cp -r readtheorg ./public/readtheorg/
 
 cp CNAME ./public/CNAME
 cp Artwork.png ./public/Artwork.png
@@ -22,4 +22,5 @@ cd public
 git init
 git add .
 git commit -m "Deploy to Github Pages"
-git push -f "https://${GH_TOKEN}@${GH_REF}" master:gh-pages
+git remote add origin git@github.com:emacs-china/Spacemacs-rocks.git
+git push  -f origin master:gh-pages
